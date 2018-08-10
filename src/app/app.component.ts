1a494575ad08a96ab17585221229712a852e8fa5
unknown <ashishuiux@gmail.com>
2018-08-10 15:39:01 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+/* This is the component similar to the React Component
+    selector => how the component if referred to else-where (unique-identifier)
+    templateUrl => Where to grab the html for this component
+    stylesUrls => The css for this component
+*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
+// We then export the component with a title property
 export class AppComponent {
   title = 'ng6-proj';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
+/* This module is something you'll need to come back to time to time when adding packages etc. */
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
+
+/* 
+  Declarations - components
+  Providers - services
+  imports -
+
+*/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
